a2e54ae34c9b79d68c1e32384c240ce050615406
Carrie Mathieu <carrieburks@gmail.com>
2020-08-09 16:50:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{array.join(", ")}"
+    puts "The line is currently: #{array.join(" 1. ")}"
     #count = 1
     #array.each_with_index do |name|
      # puts "The line is currently: #{count}. #{name}"
